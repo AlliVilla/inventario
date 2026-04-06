f08d458c15260c87b1f11bffe0d5a82a1e7a22e1
AlliVilla <villamila@unitec.edu>
2026-04-06 12:16:22 -0600

    Update config.js
    probando el deploy

--- ferreteriaVillamil-main/ferreteriaVillamil-main/backend/config/config.js
+++ ferreteriaVillamil-main/ferreteriaVillamil-main/backend/config/config.js
@@ -27,10 +27,14 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "postgres"
+    use_env_variable: 'DATABASE_URL',
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 }
